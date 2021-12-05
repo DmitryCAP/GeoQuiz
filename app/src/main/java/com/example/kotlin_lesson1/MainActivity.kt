4cb0a606e3b65f13c094523a7eb641fbf65c6f4d
DmitryCAP <dmitriidrozdov3@gmail.com>
2021-12-06 01:16:37 +0700

    create APP

--- app/src/main/java/com/example/kotlin_lesson1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lesson1/MainActivity.kt
@@ -1,36 +1,58 @@
 package com.example.kotlin_lesson1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
-private const val TAG = "MainActivity"
+private var RESULT_TEST = "Mail"
+
+var resultTest: Int = 0
 
 
 class MainActivity : AppCompatActivity() {
+lateinit var resultTestView: String
+    lateinit var sendResultButton: Button
+
     private lateinit var Button1: Button
     private lateinit var Button2: Button
     private lateinit var Button3: Button
-        private lateinit var nextButton: Button
+    private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
     private val questionBank = listOf(
         Question(R.string.question1, 1),
         Question(R.string.question2, 2),
-        Question(R.string.question3,1 ),
-        Question(R.string.question4,2 ),
-        Question(R.string.question5,2 ),
+        Question(R.string.question3, 1),
+        Question(R.string.question4, 2),
+        Question(R.string.question5, 2),
 
-    )
+        )
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+
+
+
+        sendResultButton = findViewById(R.id.send_result_button)
+
+        sendResultButton.setOnClickListener {
+
+            val sendMailIntent: Intent = Intent(this, SendMailActivity::class.java)
+
+            sendMailIntent.putExtra(RESULT_TEST, "количество правильных ответов: $resultTest")
+
+            startActivity(sendMailIntent)
+        }
+//        resultTestView =
+//        resultTestView = "количество правильных ответов: $resultTest"
+
         Button1 =
             findViewById(R.id.button1)
         Button2 =
@@ -51,54 +73,58 @@ class MainActivity : AppCompatActivity() {
         Button3.setOnClickListener { view: View ->
             checkAnswer(3)
         }
-            nextButton.setOnClickListener {
-                currentIndex = (currentIndex + 1) %
-                        questionBank.size
+        nextButton.setOnClickListener {
+
+            if (currentIndex < 4) {
+                Button1.isClickable = true
+                Button2.isClickable = true
+                Button3.isClickable = true
+
+                currentIndex = (currentIndex + 1)
                 updateQuestion()
             }
-            val questionTextResId = questionBank[currentIndex].textResId
-            updateQuestion()
-
+            else
+                nextButton.isClickable = false
         }
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume() called")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
-    }
-        private fun updateQuestion() {
-            val questionTextResId = questionBank[currentIndex].textResId
-            questionTextView.setText(questionTextResId)
+        val questionTextResId = questionBank[currentIndex].textResId
+        updateQuestion()
 
-        }
-
-        private fun checkAnswer(userAnswer: Int) {
-            val correctAnswer =
-                questionBank[currentIndex].answer
-            val messageResId = if (userAnswer == correctAnswer) {
-                R.string.correct_toast
-            }
-            else {
-                R.string.incorrect_toast
-            }
-            Toast.makeText(
-                this, messageResId,
-                Toast.LENGTH_SHORT
-            )
-                .show()
-        }
     }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+
+    }
+
+    private fun checkAnswer(userAnswer: Int) {
+
+
+        val correctAnswer =
+            questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            Button1.isClickable = false
+            Button2.isClickable = false
+            Button3.isClickable = false
+            resultTest += 1
+
+            R.string.correct_toast
+
+        } else {
+            Button1.isClickable = false
+            Button2.isClickable = false
+            Button3.isClickable = false
+
+            R.string.incorrect_toast
+        }
+        Toast.makeText(
+            this, messageResId,
+            Toast.LENGTH_SHORT
+        )
+
+            .show()
+
+
+    }
+
+}
--- app/src/main/java/com/example/kotlin_lesson1/SendMailActivity.kt
+++ app/src/main/java/com/example/kotlin_lesson1/SendMailActivity.kt
@@ -1,11 +1,34 @@
 package com.example.kotlin_lesson1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
+private var RESULT_TEST = "Mail"
 class SendMailActivity : AppCompatActivity() {
+    lateinit var openMailButton: Button
+
+
+    lateinit var textMail: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_mail)
+
+        openMailButton = findViewById(R.id.open_mail_button)
+
+
+        openMailButton.setOnClickListener {
+            val mailLink = Uri.parse("https://yandex.mail.ru/messages#sentmsg")
+            val openMailIntent = Intent(Intent.ACTION_VIEW, mailLink)
+            startActivity(openMailIntent)
+
+            textMail = findViewById(R.id.text_mail)
+            val resultTestView: String? = intent.extras?.getString(RESULT_TEST, "0")
+            textMail.text = resultTestView
+        }
     }
 }
